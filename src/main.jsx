6eec5afaba0ac75e134e508ce0543fd0c56e6ed0
pennjay237 <pennjude31@gmail.com>
2025-02-14 05:01:15 +0100

    my first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDom from 'react-dom';
+import App from '.App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDom.render(
+    <React.Strctmode>
+      <App/>
+    </React.Strctmode>,
+    document.getElementById('root')
 )
